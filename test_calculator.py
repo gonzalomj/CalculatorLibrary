5af2583aa8ba66b649d2fd4e200cbdd7fdb17db5
Gonzalomj <gonpubgh@gmail.com>
2021-10-29 17:25:21 +0200

    also extra

--- test_calculator.py
+++ test_calculator.py
@@ -13,3 +13,6 @@ class TestCalculator:
     
     def test_multiplication(self):
         assert 100 == calculator.multiply(10, 10)
+    
+    def test_division(self):
+        assert 1 == calculator.division(2, 2)
